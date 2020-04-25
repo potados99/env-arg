1d8fdd6f197667294bca58a4f6c2ff4df18ce450
potados99 <potados99@gmail.com>
2020-04-25 21:22:35 +0900

    v0.0.3

--- index.mjs
+++ index.mjs
@@ -1,12 +1,12 @@
 import minimist from 'minimist';
 
-function getEnv(key, fallback=null) {
+export function getEnv(key, fallback=null) {
     const allEnvArgs = process.env;
 
     return allEnvArgs[key] || fallback;
 }
 
-function getArg(key, fallback=null) {
+export function getArg(key, fallback=null) {
     const allArgs = minimist(process.argv.slice(2));
 
     return allArgs[key] || fallback;
